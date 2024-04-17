97e6232d6acdb934bc32c0d02d528e53cf26d9f7
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-04-17 12:13:34 +0200

    Se añade un recuadro para los planos

--- vue_timetable/src/components/AdminPage.vue
+++ vue_timetable/src/components/AdminPage.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useRouter } from 'vue-router';
-import { cargarXml,cargarCsvAlumnos } from '@/api/peticiones'
+import { cargarXml,cargarCsvAlumnos, cargarCsvPlanos } from '@/api/peticiones'
 import { ref, watch } from 'vue';
 //Instancia del router
 const router = useRouter();
@@ -14,6 +14,7 @@ body.style.margin = 0;
 const colorFichero = ref("");
 const infoFicheroXml = ref("");
 const infoFicheroCsvStudents = ref("");
+const infoFicheroCsvPlanos = ref("");
 const recarga = ref(true);
 
 //Metodos
@@ -30,6 +31,7 @@ const cargarCentro = ()=>{
     if (typeof fileName.files[0]=="undefined")
     {
         infoFicheroCsvStudents.value = "";
+        infoFicheroCsvPlanos.value = "";
         infoFicheroXml.value = "No se ha seleccionado ningun fichero";
         //Acceso al estilo
         colorFichero.value = "color:darkgoldenrod;";
@@ -62,6 +64,7 @@ const cargarAlumnos = ()=>{
     if(typeof fileName.files[0]=="undefined")
     {
         infoFicheroXml.value = "";
+        infoFicheroCsvPlanos.value = "";
         infoFicheroCsvStudents.value = "No se ha seleccionado ningun fichero";
         //Acceso al estilo
         colorFichero.value = "color:darkgoldenrod;";
@@ -82,6 +85,36 @@ const cargarAlumnos = ()=>{
     }
 }
 
+const cargarPlanos = () =>{
+    //Obtenemos el elemento input file por su id
+    const fileName = document.getElementById("planos");
+
+    //Si no encuentra ningun fichero cargado mostramos un warning si no mandamos el fichero
+    if(typeof fileName.files[0]=="undefined")
+    {
+        infoFicheroXml.value = "";
+        infoFicheroCsvStudents.value = "";
+        infoFicheroCsvPlanos.value = "No se ha seleccionado ningun fichero";
+        //Acceso al estilo
+        colorFichero.value = "color:darkgoldenrod;";
+        //Llamada a la recarga de la pagina
+        recarga.value = false;
+    }
+    else
+    {
+
+        //Instanciamos un fichero de tipo .*
+        let file = new FormData();
+
+        //Le asignamos el fichero cargado
+        file.append('csvFile',fileName.files[0]);
+
+        //Mandamos el fichero y su nombre
+        cargarDatosPlanos(file,fileName.files[0].name);
+    }
+    
+}
+
 /**
  * Metodo que llama a la peticion donde se envia el fichero xml
  * que el administrador proporciona al servidor, ademas avisa al 
@@ -97,6 +130,7 @@ const cargarDatosCentro = async (file,fileName) =>{
     if(data)
     {
         infoFicheroCsvStudents.value = "";
+        infoFicheroCsvPlanos.value = "";
         infoFicheroXml.value = "Fichero "+fileName+" cargado correctamente";
         //Acceso al estilo
         colorFichero.value = "color:forestgreen;";
@@ -105,6 +139,7 @@ const cargarDatosCentro = async (file,fileName) =>{
     }
     else
     {
+        infoFicheroCsvPlanos.value = "";
         infoFicheroCsvStudents.value = "";
         infoFicheroXml.value = "El fichero cargado es erroneo, comprueba que contiene la estructura correcta del centro";
         //Llamada al estilo
@@ -129,6 +164,7 @@ const cargarDatosAlumnos = async (file,fileName) => {
     if(data)
     {
         infoFicheroXml.value = "";
+        infoFicheroCsvPlanos.value = "";
         infoFicheroCsvStudents.value = "Fichero "+fileName+" cargado correctamente";
         //Acceso al estilo
         colorFichero.value = "color:forestgreen;";
@@ -138,7 +174,40 @@ const cargarDatosAlumnos = async (file,fileName) => {
     else
     {
         infoFicheroXml.value = "";
+        infoFicheroCsvPlanos.value = "";
         infoFicheroCsvStudents.value = "El fichero cargado es erroneo, comprueba que la cabecera del fichero sea Alumno/a y Unidad o Curso y que los alumnos esten bien formados";
+        //Acceso al estilo
+        colorFichero.value = "color:darkred;";
+        //Llamada a la recarga de la pagina
+        recarga.value = false;
+    }
+}
+/**
+ * Metodo que llama a la peticion donde se envia el fichero csv
+ * que el administrador proporciona al servidor, ademas avisa al 
+ * administrador si el fichero esta bien formado o no
+ * @param {FormData} file 
+ * @param {string} fileName 
+ */
+const cargarDatosPlanos = async(file,fileName) =>{
+    //Llamada a a peticion que devuelve un booleano
+    const data = await cargarCsvPlanos(file);
+
+    if(data)
+    {
+        infoFicheroXml.value = "";
+        infoFicheroCsvStudents.value = "";
+        infoFicheroCsvPlanos.value = "Fichero "+fileName+" cargado correctamente";
+        //Acceso al estilo
+        colorFichero.value = "color:forestgreen;";
+        //Llamada a la recarga de la pagina
+        recarga.value = false;
+    }
+    else
+    {
+        infoFicheroXml.value = "";
+        infoFicheroCsvStudents.value = "";
+        infoFicheroCsvPlanos.value = "El fichero cargado es erroneo, comprueba que la cabecera del fichero sea height,width,top,right,left,planta,numIntAu,abreviatura,nombre y que los planos esten bien formados";
         //Acceso al estilo
         colorFichero.value = "color:darkred;";
         //Llamada a la recarga de la pagina
@@ -201,6 +270,14 @@ watch(recarga,(nuevo,viejo)=>{
                         <h3 v-bind:style="colorFichero" v-show="infoFicheroCsvStudents!=''">{{infoFicheroCsvStudents}}</h3>
                     </div>
                     <button id="alinear" v-on:click="cargarAlumnos()">Cargar</button>
+                </section>
+                <section class="seccion1">
+                    <label for="Planos en csv">Cargar planos en csv:</label>
+                    <div class="marco_interno">
+                        <input type="file" id="planos" placeholder="Planos..." accept=".csv">
+                        <h3 v-bind:style="colorFichero" v-show="infoFicheroCsvPlanos!=''">{{infoFicheroCsvPlanos}}</h3>
+                    </div>
+                    <button id="alinear" v-on:click="cargarPlanos()">Cargar</button>
                 </section>
             </div>
 </template>
