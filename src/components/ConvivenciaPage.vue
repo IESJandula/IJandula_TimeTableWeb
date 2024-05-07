2fe2eab13d1345efe3a5640b8d3742fcb7885024
PabloRuiz1312 <pruican1312@g.educaand.es>
2024-05-07 10:54:37 +0200

    Se informa del estado de los docentes de convivencia

--- src/components/ConvivenciaPage.vue
+++ src/components/ConvivenciaPage.vue
@@ -199,7 +199,7 @@ watch(recarga,(nuevo,viejo)=>{
     <div class="opcion" v-show="recarga" v-if="!errorAlumnos"> 
 
         <div>
-            <h2>Docente actualmente de guardia: <span id="docenteguardia"><!--Aqui se integra el maestro de guardia en ese momento --> Valor automatico. </span></h2>
+            <h2>Docente actualmente de guardia: <span id="docenteguardia"><!--Aqui se integra el maestro de guardia en ese momento --> No disponible en este momento </span></h2>
         </div>
 
         <div class="contenido">
