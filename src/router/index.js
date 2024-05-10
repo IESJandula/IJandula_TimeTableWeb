d31f12f6460359cd1e0a4ce1023ea60c99f5f492
PabloE-03 <PabloRuiz1312>
2024-05-10 09:19:06 +0200

    Cambio de la ruta principal

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/horarios',
+      path: '/',
       name: 'welcome page',
       component: HomeView
     },
